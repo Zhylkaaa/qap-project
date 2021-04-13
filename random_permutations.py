e41335c31bfee1d53e07b9fec281732cc9088700
Dima <zhylko.dima@gmail.com>
2021-04-13 15:39:43 +0200

    wops! change parameters order

--- random_permutations.py
+++ random_permutations.py
@@ -7,12 +7,12 @@ if __name__ == '__main__':
     n, dists, costs = load_example('data/qapdata/kra30a.dat')
     _, opt, permutation = load_solution('data/qapsoln/kra30a.sln')
 
-    assert opt == objective(permutation, dists, costs)
+    assert opt == objective(dists, costs, permutation)
 
     np.random.seed(1234)
     diffs = []
     for _ in range(1000):
-        diffs.append(objective(np.random.permutation(n), dists, costs) - opt)
+        diffs.append(objective(dists, costs, np.random.permutation(n)) - opt)
 
     print(
         f'mean difference for random permutations is {np.mean(diffs)}, min {np.min(diffs)}, max {np.max(diffs)}\n'
