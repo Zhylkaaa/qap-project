89ef6064a06f7c7ef90c3d302913f6d015cdb3c9
Dima <zhylko.dima@gmail.com>
2021-04-29 13:43:17 +0200

    remove TODO in bees_solver

--- QAP/solvers/bees/bees_solver.py
+++ QAP/solvers/bees/bees_solver.py
@@ -1,4 +1,3 @@
-# TODO: implement bees algorithm to solve QAP
 # papers:
 # * https://www.researchgate.net/publication/260985621_The_Bees_Algorithm_Technical_Note
 # * https://link.springer.com/chapter/10.1007/978-3-319-23437-3_53
